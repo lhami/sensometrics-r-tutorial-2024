4a1ce2f88fbc9841d29e3cefa178e4647f5a947c
Leah M. Hamilton <lhamilton@vsu.edu>
2024-03-20 18:31:25 -0400

    first draft of pre-workshop tutorial

---
+++ code/environmenttest.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(ca)
+library(FactoMineR)
+library(ggrepel)
+library(factoextra)
+#To debug your R version number:
+#sessionInfo()$R.version$version.string
+#To debug your working directory:
+#getwd()
+
+berry_data <- read_csv("data/clt-berry-data.csv")
+berry_data %>%
+  select(-where(~ any(is.na(.x)))) %>%
+  group_by(`Sample Name`) %>%
+  summarize(across(.cols = starts_with("cata_"), .fns = sum)) %>%
+  column_to_rownames("Sample Name") %>%
+  ca() -> berry_ca
+
+berry_ca$sv[1]
